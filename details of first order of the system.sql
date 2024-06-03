78dee2d404ff14e6fdcc21d3968ed9e62adfb522
Dev patil <105635790+Devpatil8878@users.noreply.github.com>
2024-06-03 09:08:31 +0530

    Update details of first order of the system.sql

--- details of first order of the system.sql
+++ details of first order of the system.sql
@@ -1,5 +1,2 @@
-SELECT soh.SalesOrderID, soh.OrderDate, c.CustomerID, p.FirstName, p.LastName
-FROM Sales.SalesOrderHeader AS soh
-JOIN Sales.Customer AS c ON soh.CustomerID = c.CustomerID
-JOIN Person.Person AS p ON c.PersonID = p.BusinessEntityID
-WHERE soh.OrderDate = (SELECT MIN(OrderDate) FROM Sales.SalesOrderHeader);
+select top 1 * from SalesLT.SalesOrderHeader
+order by OrderDate
