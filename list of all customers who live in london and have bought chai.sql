b59470ad3d98388f148b134808edc1e4dd057a3f
Dev patil <105635790+Devpatil8878@users.noreply.github.com>
2024-06-03 08:15:45 +0530

    Update list of all customers who live in london and have bought chai.sql

--- list of all customers who live in london and have bought chai.sql
+++ list of all customers who live in london and have bought chai.sql
@@ -1,8 +1,7 @@
-SELECT DISTINCT c.CustomerID, p.FirstName, p.LastName
-FROM Sales.Customer AS c
-JOIN Person.Person AS p ON c.PersonID = p.BusinessEntityID
-JOIN Sales.SalesOrderHeader AS soh ON c.CustomerID = soh.CustomerID
-JOIN Sales.SalesOrderDetail AS sod ON soh.SalesOrderID = sod.SalesOrderID
-JOIN Production.Product AS prod ON sod.ProductID = prod.ProductID
-JOIN Person.Address AS a ON p.BusinessEntityID = a.BusinessEntityID
-WHERE a.City = 'London' AND prod.Name = 'Chai';
+select * from SalesLT.Customer
+join SalesLT.CustomerAddress on Customer.CustomerID = CustomerAddress.CustomerID
+join SalesLT.Address on CustomerAddress.AddressID = Address.AddressID
+join SalesLT.SalesOrderHeader on Customer.CustomerID =  SalesOrderHeader.CustomerID
+join SalesLT.SalesOrderDetail on SalesOrderHeader.SalesOrderID = SalesOrderDetail.SalesOrderID
+join SalesLT.Product on SalesOrderDetail.ProductID = Product.ProductID
+where Address.City = 'London' and Product.Name = 'chai'
