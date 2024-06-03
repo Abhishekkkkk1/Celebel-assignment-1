5b93e590327b90eca5e3d014772ab4cbd1019872
Dev patil <105635790+Devpatil8878@users.noreply.github.com>
2024-06-03 11:28:16 +0530

    Update Get a list of all managers and total number of employees who report to them.sql

--- Get a list of all managers and total number of employees who report to them.sql
+++ Get a list of all managers and total number of employees who report to them.sql
@@ -1,7 +1,6 @@
-SELECT m.BusinessEntityID AS ManagerID, 
-       m.FirstName AS ManagerFirstName, 
-       m.LastName AS ManagerLastName,
-       COUNT(e.BusinessEntityID) AS TotalEmployees
-FROM HumanResources.Employee AS m
-LEFT JOIN HumanResources.Employee AS e ON m.BusinessEntityID = e.ManagerID
-GROUP BY m.BusinessEntityID, m.FirstName, m.LastName;
+
+SELECT m.BusinessEntityID AS ManagerID, p.FirstName, p.LastName, COUNT(e.BusinessEntityID) AS NumberOfReports
+FROM HumanResources.Employee e
+JOIN HumanResources.Employee m ON e.OrganizationNode = m.OrganizationNode.GetAncestor(1)
+JOIN Person.Person p ON m.BusinessEntityID = p.BusinessEntityID
+GROUP BY m.BusinessEntityID, p.FirstName, p.LastName;
