c083002d2bcddd24319c57f768f217b9f3932bbd
Dev patil <105635790+Devpatil8878@users.noreply.github.com>
2024-06-03 09:11:46 +0530

    Update find the details of most expensive order date.sql

--- find the details of most expensive order date.sql
+++ find the details of most expensive order date.sql
@@ -1,5 +1,3 @@
-SELECT soh.SalesOrderID, soh.OrderDate, c.CustomerID, p.FirstName, p.LastName, soh.TotalDue
-FROM Sales.SalesOrderHeader AS soh
-JOIN Sales.Customer AS c ON soh.CustomerID = c.CustomerID
-JOIN Person.Person AS p ON c.PersonID = p.BusinessEntityID
-WHERE soh.TotalDue = (SELECT MAX(TotalDue) FROM Sales.SalesOrderHeader);
+select top 1 * from SalesLT.SalesOrderHeader
+join SalesLT.SalesOrderDetail on SalesOrderDetail.SalesOrderID = SalesOrderHeader.SalesOrderID
+order by SalesOrderDetail.UnitPrice DESC
