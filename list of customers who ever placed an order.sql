c5d86c2042fee910dec3a368d477766d006e181f
Dev patil <105635790+Devpatil8878@users.noreply.github.com>
2024-06-03 07:54:40 +0530

    Update list of customers who ever placed an order.sql

--- list of customers who ever placed an order.sql
+++ list of customers who ever placed an order.sql
@@ -1,4 +1,2 @@
-SELECT DISTINCT c.CustomerID, p.FirstName, p.LastName
-FROM Sales.Customer AS c
-JOIN Person.Person AS p ON c.PersonID = p.BusinessEntityID
-JOIN Sales.SalesOrderHeader AS soh ON c.CustomerID = soh.CustomerID;
+select * from SalesLT.Customer
+right join SalesLT.SalesOrderHeader on Customer.CustomerID = SalesOrderHeader.CustomerID
